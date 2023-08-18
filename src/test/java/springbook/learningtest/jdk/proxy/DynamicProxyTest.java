b1c5edbb1d44208ca88946e370fba139954ca609
IMWoo94 <leemeawoo94@gmail.com>
2023-08-18 19:04:43 +0900

    2023.08.18 study spring support ProxyFactroyBean

---
+++ src/test/java/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -0,0 +1,85 @@
+package springbook.learningtest.jdk.proxy;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import java.lang.reflect.Proxy;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+
+import springbook.learningtest.jdk.Hello;
+import springbook.learningtest.jdk.HelloTarget;
+import springbook.user.handler.UppercaseHandler;
+
+public class DynamicProxyTest {
+
+	@Test
+	@DisplayName("Java 제공 JDK Dynamic Proxy")
+	public void simpleProxy() {
+
+		Hello proxiedHello = (Hello)Proxy.newProxyInstance(
+			getClass().getClassLoader(),
+			new Class[] {Hello.class},
+			new UppercaseHandler(new HelloTarget())
+		);
+
+		Hello noProxiedHello = new HelloTarget();
+
+		System.out.println("jdk dynamic proxy : " + proxiedHello.getClass());
+		System.out.println("smple new object : " + noProxiedHello.getClass());
+
+		System.out.println(proxiedHello.sayHello("proxyHello"));
+		System.out.println(noProxiedHello.sayHello("proxyHello"));
+
+	}
+
+	@Test
+	@DisplayName("spring 제공 ProxyFactoryBean")
+	public void proxyFactroyBean() {
+		ProxyFactoryBean pfBean = new ProxyFactoryBean();
+		// 타켓 설정
+		pfBean.setTarget(new HelloTarget());
+
+		// 부가 기능(어드바이스)를 추가 한다. 여러 개 추가 가능
+		pfBean.addAdvice(new UppercaseAdvice());
+		pfBean.addAdvice(new ReplaceAdvice());
+
+		// 굳이 지정하지 않아도 자동검출 기능을 활용해 타겟의 오브젝트가 구현하는 인터페이스 정보를 알아낸다.
+		pfBean.setInterfaces(Hello.class);
+
+		Hello proxiedHello = (Hello)pfBean.getObject();
+		System.out.println(proxiedHello.getClass());
+		assertThat(proxiedHello.sayHello("Toby"), is("HELLO LEE"));
+		assertThat(proxiedHello.sayHi("Toby"), is("HI LEE"));
+		assertThat(proxiedHello.sayThankYou("Toby"), is("THANKYOU LEE"));
+
+	}
+
+	static class UppercaseAdvice implements MethodInterceptor {
+		@Override
+		public Object invoke(MethodInvocation invocation) throws Throwable {
+			System.out.println("UppercaseAdvice start");
+			String ret = (String)invocation.proceed();
+			System.out.println("UppercaseAdvice end");
+			return ret.toUpperCase();
+		}
+	}
+
+	static class ReplaceAdvice implements MethodInterceptor {
+
+		@Override
+		public Object invoke(MethodInvocation invocation) throws Throwable {
+			System.out.println("ReplaceAdvice start");
+			Object object = invocation.proceed();
+			System.out.println("ReplaceAdvice end");
+			if (object instanceof String) {
+				return ((String)object).replace("Toby", "lee");
+			}
+			return object;
+		}
+	}
+}
